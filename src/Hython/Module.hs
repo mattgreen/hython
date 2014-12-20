cf94fe1a7f5443fd3f0058f9b2eb2478f8f16299
Matt Green <mattgreenrocks@gmail.com>
2014-12-20 11:36:50 -0500

    Only import modules once

--- src/Hython/Module.hs
+++ src/Hython/Module.hs
@@ -2,6 +2,7 @@ module Hython.Module (loadModule)
 where
 
 import Control.Monad.State
+import Data.List
 import System.FilePath.Posix
 
 import Language.Python.Core
@@ -11,25 +12,30 @@ import Hython.InterpreterState
 
 loadModule :: String -> (String -> AttributeDict -> Interpreter ()) -> Interpreter Module
 loadModule importPath action = do
-    current <- gets currentModule
-    dict    <- liftIO AttributeDict.empty
+    current         <- gets currentModule
+    loadedModules   <- gets modules
 
     let resolvedPath = resolveModulePath importPath (modulePath current)
 
-    code <- liftIO $ readFile resolvedPath
+    case lookupModuleByPath resolvedPath loadedModules of
+        Just m  -> return m
+        Nothing -> do
+            code <- liftIO $ readFile resolvedPath
+            dict <- liftIO AttributeDict.empty
 
-    let newModule = Module {
-        moduleName = newModuleName,
-        modulePath = resolvedPath,
-        moduleDict = dict
-    }
+            let newModule = Module {
+                moduleName = newModuleName,
+                modulePath = resolvedPath,
+                moduleDict = dict
+            }
 
-    modify $ \s -> s { currentModule = newModule }
-    action code dict
-    modify $ \s -> s { currentModule = current }
+            modify $ \s -> s { currentModule = newModule }
+            action code dict
+            modify $ \s -> s { currentModule = current }
 
-    return newModule
+            return newModule
   where
+    lookupModuleByPath resolvedPath = find (\m -> modulePath m == resolvedPath)
     newModuleName = takeBaseName importPath
 
 resolveModulePath :: String -> FilePath -> FilePath
--- test/import.py
+++ test/import.py
@@ -1,4 +1,5 @@
 import imported
+import imported
 
 print("OK")
 print(imported.value)
