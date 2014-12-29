52eb30a9afb8fd5a7d65a4cc96721fba47b570d9
Matt Green <mattgreenrocks@gmail.com>
2014-12-29 16:05:03 -0500

    Print usage if no CLI args provided

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,16 @@
 import System.Environment
+import System.Exit
 
 import Hython.Interpreter
 
 main :: IO ()
 main = do
-    [filename]  <- getArgs
-    code        <- readFile filename
+    args <- getArgs
+    case args of
+        [filename]  -> do
+            code <- readFile filename
+            interpret filename code
 
-    interpret filename code
+        _           -> do
+            putStrLn "Usage: hython <filename>"
+            exitFailure
