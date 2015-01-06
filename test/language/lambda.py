dd58a46eb3cb9c4879e6f4a0c698d9ab2d5165a3
Matt Green <mattgreenrocks@gmail.com>
2015-01-06 16:32:11 -0500

    Some more lambda expression tests

--- test/language/lambda.py
+++ test/language/lambda.py
@@ -4,3 +4,12 @@ print(simple(42))
 abc = 42
 adds_enclosing = lambda x: x + abc
 print(adds_enclosing(18))
+
+uses_other_lambdas = lambda x, y: simple(x) + adds_enclosing(y)
+print(uses_other_lambdas(2, 99))
+
+def returns_lambda():
+    x = 12
+    return lambda a: a + x
+
+print(returns_lambda()(77))
