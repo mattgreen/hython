eda833d48745c61c57018456396a20f9eb735df9
Matt Green <mattgreenrocks@gmail.com>
2015-02-01 17:51:18 -0500

    Split built-in exceptions into their own module

--- lib/builtins.py
+++ lib/builtins.py
@@ -1,75 +1,9 @@
-class BaseException(object):
-    def __init__(self, message):
-        self._message = message
+from exceptions import *
 
-    def __str__(self):
-        return self._message
-
-class SystemExit(BaseException):
+class NotImplementedType(object):
     pass
 
-class KeyboardInterrupt(BaseException):
-    pass
-
-class GeneratorExit(BaseException):
-    pass
-
-class Exception(BaseException):
-    pass
-
-class StopIteration(Exception):
-    pass
-
-class ArithmeticError(Exception):
-    pass
-
-class AssertionError(Exception):
-    pass
-
-class AttributeError(Exception):
-    pass
-
-class BufferError(Exception):
-    pass
-
-class EOFError(Exception):
-    pass
-
-class ImportError(Exception):
-    pass
-
-class LookupError(Exception):
-    pass
-
-class MemoryError(Exception):
-    pass
-
-class NameError(Exception):
-    pass
-
-class OSError(Exception):
-    pass
-
-class ReferenceError(Exception):
-    pass
-
-class RuntimeError(Exception):
-    pass
-
-class SyntaxError(Exception):
-    pass
-
-class SystemError(Exception):
-    pass
-
-class TypeError(Exception):
-    pass
-
-class ValueError(Exception):
-    pass
-
-class Warning(Exception):
-    pass
+NotImplemented = NotImplementedType()
 
 def isinstance(o, cls):
     return issubclass(o.__class__, cls)
---
+++ lib/exceptions.py
@@ -0,0 +1,72 @@
+class BaseException(object):
+    def __init__(self, message):
+        self._message = message
+
+    def __str__(self):
+        return self._message
+
+class SystemExit(BaseException):
+    pass
+
+class KeyboardInterrupt(BaseException):
+    pass
+
+class GeneratorExit(BaseException):
+    pass
+
+class Exception(BaseException):
+    pass
+
+class StopIteration(Exception):
+    pass
+
+class ArithmeticError(Exception):
+    pass
+
+class AssertionError(Exception):
+    pass
+
+class AttributeError(Exception):
+    pass
+
+class BufferError(Exception):
+    pass
+
+class EOFError(Exception):
+    pass
+
+class ImportError(Exception):
+    pass
+
+class LookupError(Exception):
+    pass
+
+class MemoryError(Exception):
+    pass
+
+class NameError(Exception):
+    pass
+
+class OSError(Exception):
+    pass
+
+class ReferenceError(Exception):
+    pass
+
+class RuntimeError(Exception):
+    pass
+
+class SyntaxError(Exception):
+    pass
+
+class SystemError(Exception):
+    pass
+
+class TypeError(Exception):
+    pass
+
+class ValueError(Exception):
+    pass
+
+class Warning(Exception):
+    pass
