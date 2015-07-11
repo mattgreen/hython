3fca63511e4c3b32a9cf5be28ea66590345b27f0
Matt Green <mattgreenrocks@gmail.com>
2015-07-11 10:15:05 -0400

    Flesh out evalExpr (Constant)

--- src/Hython/Monad.hs
+++ src/Hython/Monad.hs
@@ -1,6 +1,9 @@
 module Hython.Monad
 where
 
+import qualified Data.ByteString.Char8 as B
+import Data.Complex
+
 import Language.Python
 
 import Hython.Name
@@ -15,3 +18,23 @@ class Monad m => MonadInterpreter m where
 
     evalBlock       :: [Statement] -> m [Object]
 
+newNone :: MonadInterpreter m => m Object
+newNone = return None
+
+newBool :: MonadInterpreter m => Bool -> m Object
+newBool b = return $ Bool b
+
+newBytes :: MonadInterpreter m => String -> m Object
+newBytes b = return $ Bytes (B.pack b)
+
+newFloat :: MonadInterpreter m => Double -> m Object
+newFloat d = return $ Float d
+
+newImag :: MonadInterpreter m => Complex Double -> m Object
+newImag i = return $ Imaginary i
+
+newInt :: MonadInterpreter m => Integer -> m Object
+newInt i = return $ Int i
+
+newString :: MonadInterpreter m => String -> m Object
+newString s = return $ String s
--- src/Hython/Object.hs
+++ src/Hython/Object.hs
@@ -1,7 +1,18 @@
 module Hython.Object
 where
 
+import Data.ByteString
+import Data.Complex
 import Data.IORef
 
-data Object = Object
+data Object = None
+            | Bool Bool
+            | Bytes ByteString
+            | Float Double
+            | Imaginary (Complex Double)
+            | Int Integer
+            | String String
+            deriving (Eq, Show)
+
 type ObjectRef = IORef Object
+
