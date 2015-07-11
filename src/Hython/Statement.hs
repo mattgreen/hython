f3a026502a3bc50b7e2ed6f31074de449a95f002
Matt Green <mattgreenrocks@gmail.com>
2015-07-11 10:27:49 -0400

    Assignment statement & name lookup

--- src/Hython/Statement.hs
+++ src/Hython/Statement.hs
@@ -1,6 +1,8 @@
 module Hython.Statement (eval)
 where
 
+import Data.Text
+
 import Language.Python
 
 import Hython.Expression
@@ -8,6 +10,11 @@ import Hython.Monad
 import Hython.Object
 
 eval :: MonadInterpreter m => Statement -> m (Maybe Object)
+eval (Assignment (Name name) expr) = do
+    value <- evalExpr expr
+    bind (pack name) value
+    return Nothing
+
 eval (Expression e) = do
     obj <- evalExpr e
     return $ Just obj
