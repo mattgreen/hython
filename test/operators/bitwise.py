3b53461527b7a7558cc55e05ffd560f483478bd5
Matthias Heinzel <mheinzel@posteo.de>
2016-08-09 23:46:49 +0200

    Add test for multiple bitwise operators

--- test/operators/bitwise.py
+++ test/operators/bitwise.py
@@ -4,3 +4,11 @@ print(83 | 7)
 print(22 ^ 5)
 print(2 << 4)
 print(16 >> 2)
+
+# multiple
+print(~~19)
+print(197 & 92 & 345)
+print(197 | 92 | 345)
+print(197 ^ 92 ^ 345)
+print(1   << 2 << 3)
+print(256 >> 4 >> 3)
